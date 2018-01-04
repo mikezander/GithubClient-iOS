be0fea6d0f3c602fa62c81a08c823345ba213079
mikezander <mikezander87@yahoo.com>
2018-01-04 08:30:07 -0500

    setup repo model

---
+++ github-client/Model/Repo.swift
@@ -0,0 +1,29 @@
+//
+//  Repo.swift
+//  github-client
+//
+//  Created by Michael Alexander on 1/4/18.
+//  Copyright © 2018 Michael Alexander. All rights reserved.
+//
+
+import UIKit
+
+class Repo {
+    public private(set) var image: UIImage
+    public private(set) var name: String
+    public private(set) var description: String
+    public private(set) var numberOfForks: Int
+    public private(set) var language: String
+    public private(set) var numberOfContributors: Int
+    public private(set) var repoUrl: String
+    
+    init(image: UIImage, name: String, description: String, numberOfForks: Int, language: String, numberOfContributors: Int, repoUrl: String) {
+        self.image = image
+        self.name = name
+        self.description = description
+        self.numberOfForks = numberOfForks
+        self.language = language
+        self.numberOfContributors = numberOfContributors
+        self.repoUrl = repoUrl
+    }
+}
