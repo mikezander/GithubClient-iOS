141903234d310a8caa9349c33c16b063b50819be
mikezander <mikezander87@yahoo.com>
2018-01-05 09:55:24 -0500

    added refresh control

--- github-client/Controller/TrendingFeedVC.swift
+++ github-client/Controller/TrendingFeedVC.swift
@@ -14,18 +14,24 @@ class TrendingFeedVC: UIViewController{
 
     @IBOutlet weak var tableView: UITableView!
     
+    let refreshControl = UIRefreshControl()
+    
     var dataSource = PublishSubject<[Repo]>()
     var disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        configureRefreshControl()
+        tableView.refreshControl = refreshControl
         fetchData()
         bindDataToTableView()
     }
 
-    private func fetchData() {
+    @objc private func fetchData() {
         DownloadService.instance.downloadTrendingRepos { (trendingRepoArray) in
             self.dataSource.onNext(trendingRepoArray)
+            self.refreshControl.endRefreshing()
         }
     }
     
@@ -33,6 +39,12 @@ class TrendingFeedVC: UIViewController{
         dataSource.bind(to: tableView.rx.items(cellIdentifier: "trendingRepoCell")) { (row, repo: Repo, cell: TrendingRepoCell) in
             cell.configureCell(repo: repo)
             }.disposed(by: disposeBag)
+    }
+    
+    private func configureRefreshControl(){
+        refreshControl.tintColor = #colorLiteral(red: 0.2784313725, green: 0.462745098, blue: 0.9019607843, alpha: 1)
+        refreshControl.attributedTitle = NSAttributedString(string: "Fetching trending Github Repos 🔥", attributes: [NSAttributedStringKey.foregroundColor: #colorLiteral(red: 0.2784313725, green: 0.462745098, blue: 0.9019607843, alpha: 1), NSAttributedStringKey.font: UIFont(name: "AvenirNext-DemiBold", size: 16.0)!])
+        refreshControl.addTarget(self, action: #selector(fetchData), for: .valueChanged)
     }
 }
 
